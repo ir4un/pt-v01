a94e2b3807c369cded03307804fe8412bc291783
ir4un <ir4un99@gmail.com>
2024-02-02 17:59:49 +0800

    First Push

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,21 @@
+import AppBar from '@mui/material/AppBar';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+function Topbar() {
+    return (
+        <div>
+            <AppBar position="relative">
+                <Toolbar>
+                    <CameraIcon sx={{ mr: 2 }} />
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Album layout
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Topbar
